25d5e5ac14e4d0093f55decd96822f175c5fa7d5
Chijioke Akuneziri <asgnorge@gmail.com>
2026-09-14 15:42:30 +0200

    php

--- testfil.php
+++ testfil.php
@@ -14,3 +14,19 @@
   </form>
 </body>
 </html>
+
+<?php
+  if (isset($_POST ["fortsett"])) 
+    {
+      $svar=$_POST ["svar"];
+	
+      if ($svar == 9)  
+        {
+          print("Riktig. 3 ganger 3 er 9 ");
+        }
+      else 
+        {
+          print("Feil. 3 ganger 3 er ikke  $svar. 3 ganger 3 er 9 ");
+        }
+    }
+?>
