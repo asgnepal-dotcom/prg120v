cf07922ea0b1d3b65e57025e3cc61a6d0895627f
Chijioke Akuneziri <asgnorge@gmail.com>
2026-09-21 13:58:17 +0200

    eksempel3

--- eksempel-3.php
+++ eksempel-3.php
@@ -14,4 +14,21 @@
   print ("<br />");
   print ("Summen er $sum <br />");
   print ("Differansen er $differanse <br />");  /* resultater skrevet ut  */
+?> <?php     /* Eksempel 5 */
+/*
+/*    Programmet deler opp en tekststreng ved bruk av oppdelingsfunksjonen explode
+*/
+  $tekst="Geir Bjarvin Universitetslektor USN";
+  
+  $del=explode (" " , $tekst);     
+  $fornavn=$del[0];     
+  $etternavn=$del[1];    
+  $yrke=$del[2];    
+  $arbeidssted=$del[3];     
+
+  print ("Hele tekststrengen er: $tekst <br /> <br />");
+  print ("Fornavn er:  $fornavn <br />");
+  print ("Etternavn er:  $etternavn <br />");
+  print ("Yrke er er:  $yrke <br />");
+  print ("Arbeidssted er:  $arbeidssted <br />");
 ?>
